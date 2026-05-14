464ac0ab803473c9fd9490456406cdaaa66985d9
danielgamble <dgamble94115@uat.edu>
2026-05-14 10:11:15 -0400

    Initial Commit

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour // I forgot the u in MonoBehaviour the first time and the application did not like it.
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World"); 
+    }
+
+    // Update is called once per frame and continues until we tell it to stop.
+    void Update()
+    {
+        
+    }
+    
+}
