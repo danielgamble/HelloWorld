443b4dc42c1c1e3d84db2a3bfbb70e65fc2a165c
danielgamble <dgamble94115@uat.edu>
2026-05-14 11:53:43 -0400

    Update HelloWorld.cs

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -9,6 +9,7 @@ public class HelloWorld : MonoBehaviour // I forgot the u in MonoBehaviour the f
     {
         Debug.Log("Hello World");
         Debug.Log("Hello Again");
+        
     }
 
     // Update is called once per frame and continues until we tell it to stop.
