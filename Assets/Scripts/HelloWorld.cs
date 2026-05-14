525ecbdeaf5680592f5de151e66ad38e8af51c06
danielgamble <dgamble94115@uat.edu>
2026-05-14 11:15:02 -0400

    Added HelloWorldBG Skybox

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -7,13 +7,14 @@ public class HelloWorld : MonoBehaviour // I forgot the u in MonoBehaviour the f
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World"); 
+        Debug.Log("Hello World");
+        Debug.Log("Hello Again");
     }
 
     // Update is called once per frame and continues until we tell it to stop.
     void Update()
     {
-        
+        Debug.Log("And Again...");
     }
     
 }
